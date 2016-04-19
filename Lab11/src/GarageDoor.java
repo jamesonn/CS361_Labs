8ee8ab44d82f01f96fd8e05d30c2ceb33ff95ce9
Nate <jamesonn@uwm.edu>
2016-04-19 12:31:19 -0500

    lab11 latest

--- Lab11/src/GarageDoor.java
+++ Lab11/src/GarageDoor.java
@@ -12,6 +12,7 @@ public class GarageDoor {
     public void open(){
         motor.on();
         lightState = true;
+        System.out.println("Opening... and light on");
         try{
             TimeUnit.SECONDS.sleep(20);
         }
@@ -19,14 +20,17 @@ public class GarageDoor {
 
         }
         lightState = false;
+        System.out.println("Light off");
     }
 
     public void close(){
         motor.on();
+        System.out.println("Closing...");
     }
 
     public void stop(){
         motor.off();
+        System.out.println("Door Stopped");
         if(doorState){
             doorState = false;
         }else{
@@ -37,8 +41,10 @@ public class GarageDoor {
     public void lightClick(){
         if(lightState){
             lightState = false;
+            System.out.println("Light off");
         }else{
             lightState = true;
+            System.out.println("Light on");
         }
     }
 
@@ -54,9 +60,17 @@ public class GarageDoor {
 
     public void onSafety(){
         motor.reverse();
+        System.out.println("Safety hit");
     }
 
     public void onLimit(){
         motor.off();
+        if(doorState){
+            doorState = false;
+            System.out.println("Garage Close.");
+        }else {
+            doorState = true;
+            System.out.println("Garage Open.");
+        }
     }
 }
